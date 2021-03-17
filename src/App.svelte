8d72a378d9804ed9b31f5d50a4712f1629eccfd9
beardage <xypress@gmail.com>
2021-03-17 00:11:27 -0700

    adding svelte starter and setting up basic todo

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+    let newItem = '';
+
+    let todoList = [
+        {content: 'test item', status: false},
+        {content: 'test item #2', status: false},
+    ];
+    
+    function addToList() {
+        todoList = [...todoList, {content: newItem, status: false}];
+        newItem = '';
+    }
+    
+</script>
+
+<input bind:value={newItem} type="text" placeholder="new todo item">
+<button on:click={addToList}>Add</button>
+
+<hr>
+
+{#each todoList as item, index}
+    <input bind:checked={item.status} type="checkbox">
+    <span class:checked={item.status}>{item.content}</span>
+    <br>
+{/each}
+
+<style>
+    .checked { color: green }
+</style>
