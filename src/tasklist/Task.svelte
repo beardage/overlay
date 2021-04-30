42591df7f7c7518cdbb4d43c08e0515910bda6e3
beardage <xypress@gmail.com>
2021-04-29 22:20:38 -0700

    matching the task styles a bit better

--- src/tasklist/Task.svelte
+++ src/tasklist/Task.svelte
@@ -12,26 +12,28 @@
     {#each task.subTasks as subTask, index (subTask.id)}
         <div class="sub-task">
             <input bind:checked={subTask.status} type="checkbox">
-                <span class:checked={subTask.status}>{subTask.content}</span>
-            {/if}
+            <span class:checked={subTask.status}>{subTask.content}</span>
         </div>
     {/each}
 </div>
 
 <style>
 .task {
-    background-color: #f2f2f2;
-    border-radius: 5px;
+    background-color: #6272a4;
+    border-radius: 3px;
+    border: 4px solid #8be9fd;
     box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
-    padding: 5px;
+    color: #f8f8f8;
+    padding: 10px;
     margin-bottom: 10px;
 }
 .sub-task {
-    background-color: #f2f2f2;
-    border-radius: 5px;
+    background-color: #6272a4;
+    border-radius: 3px;
     box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+    color: #f8f8f8;
     font-size: .8em;
-    padding: 5px;
+    padding: 10px;
     margin-left: 15px;
     margin-bottom: 10px;
 }
